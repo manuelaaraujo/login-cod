befc1ae382a6800fbd8740f685fa4a2cb97e42ad
Manuela <manuela.a@alunoescolassociais.org.br>
2024-08-08 08:51:10 -0300

    adicionamos função que armazena em array nome na tela de cadastro

--- controller.js
+++ controller.js
@@ -12,18 +12,29 @@ function acessar(){
     }
 }
 
-//  FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
-var dadosLista = [];
-
-function salvarUser(){
-    let nomeUser = document.getElementById('nomeUser').
-    value;
+// FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
  
+var dadosLista = [];
+ 
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+   
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+      //console.log(dadosLista);
+      criarlista()
         document.getElementById('nomeUser').value = "";
     }else{
         alert('Favor informar o nome para cadastro');
+    }
+}
+ 
+// FUNÇÃO PARA CRIAR LISTA
+ 
+function criarlista(){
+    let tabela = document.getElementById ('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>"
+    for(let i = 0; i <= (dadosLista.length-1) ; i++){
+        tabela += "<tr><td>" + dadosLista [i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
